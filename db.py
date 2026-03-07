5513d94752cecf03cd51ea1b97dbdc8af010468b
Your Full Name <kartikeyvashistxyz@gmail.com>
2026-03-07 16:40:03 +0530

    CREATED: customers table, products table, orders table

--- db.py
+++ db.py
@@ -14,14 +14,35 @@ cursor = conn.cursor()
 #   Here we have created a table using SQL 
 
 cursor.execute("""
-    CREATE TABLE IF NOT EXISTS sales(
+    CREATE TABLE IF NOT EXISTS customers(
+        customer_id SERIAL PRIMARY KEY,
+        customer_name VARCHAR(50),
+        city VARCHAR(50),
+        email VARCHAR(50),
+        phone VARCHAR(50)
+    )
+""")
+
+cursor.execute("""
+    CREATE TABLE IF NOT EXISTS products(
         id SERIAL PRIMARY KEY,
         product_name VARCHAR(100),
-        sale_date DATE,
-        quantity INTEGER,
+        category VARCHAR(50),
         price FLOAT
     )
 """)
+
+cursor.execute("""
+    CREATE TABLE IF NOT EXISTS orders(
+        product_id INTEGER REFERENCES products(id),
+        customer_id INTEGER REFERENCES customers(customer_id),
+        order_id SERIAL PRIMARY KEY,
+        sale_date DATE,
+        quantity INTEGER,
+        payment_method VARCHAR(50)
+    )
+""")
+
 
 conn.commit()
 conn.close()
